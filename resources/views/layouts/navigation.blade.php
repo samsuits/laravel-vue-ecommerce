db0cf1c6fbdda656830f6dcf4857184d64251e29
Sam Samala <satish.samala@ezyvet.com>
2023-05-11 19:44:05 +0530

    display user name

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -29,7 +29,7 @@
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                   />
                 </svg>
-                My Account
+                {{Auth()->user()->name}}
               </span>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -138,7 +138,7 @@
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                   />
                 </svg>
-                My Account
+                {{Auth()->user()->name}}
               </span>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
